334c5b3ea37092b3454ff7bf5bc65d29e515f809
Halit Adnan Kaleci <halitadnankaleci@gmail.com>
2026-04-11 15:27:12 +0300

    GameplayTestScene bir şeyler denendi

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -1,37 +1,10 @@
 using UnityEngine;
 using Unity.Netcode;
 
-[RequireComponent(typeof(CharacterController))]
+/// <summary>
+/// Network identity for the player. Movement and camera are handled by <see cref="ThirdPersonPlayer"/>.
+/// </summary>
+[RequireComponent(typeof(ThirdPersonPlayer))]
 public class PlayerNetwork : NetworkBehaviour
 {
-    public float speed = 6f;
-
-    private CharacterController controller;
-    private Camera cam;
-
-    void Start()
-    {
-        controller = GetComponent<CharacterController>();
-
-        // SADECE kendi playerında kamera aktif olsun
-        if (IsOwner)
-        {
-            cam = Camera.main;
-            cam.transform.SetParent(transform);
-            cam.transform.localPosition = new Vector3(0, 10, -8);
-            cam.transform.localRotation = Quaternion.Euler(40, 0, 0);
-        }
-    }
-
-    void Update()
-    {
-        if (!IsOwner) return;
-
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-
-        Vector3 move = new Vector3(h, 0, v);
-
-        controller.Move(move * speed * Time.deltaTime);
-    }
 }
